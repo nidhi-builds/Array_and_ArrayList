76885c31670ba1748c01a066421ec3eac0f98957
nidhi-builds <nidhipandit2907@gmail.com>
2025-02-03 23:11:07 +0530

    Add files via upload
    Added function to find the closest neighboring numbers
    
    - Implemented `findSmallestDifferenceIndex()` to determine two neighboring 
      numbers in an array with the smallest difference.
    - Refactored `oddEven()` to display even and odd numbers separately.
    - Improved code readability and added comments for better understanding.

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -1,26 +1,51 @@
-//ArrayFunctions.java
+// ArrayFunctions.java
 
 import java.util.*;
 import java.util.Arrays;
-class ArrayFunctions{
-	public void display(ArrayList<Integer> array){
-		System.out.println("Array is:" +array);
 
-	}
-	public void oddEven(int[] array){
-		ArrayList<Integer> even = new ArrayList<Integer>();
-		ArrayList<Integer> odd = new ArrayList<Integer>();
+class ArrayFunctions {
 
-		for(int i=0; i < array.length; i++){
-			if (array[i] % 2 == 0){
-				even.add(array[i]);		
-			}	
-			else{
-				odd.add(array[i]);	
-			}
-		
-		}
-		display(even);
-		display(odd);
-	}
+    // Method to display an ArrayList
+    public void display(ArrayList<Integer> array) {
+        System.out.println("Array is: " + array);
+    }
+
+    // Method to separate even and odd numbers into different lists
+    public void oddEven(int[] array) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+
+        for (int num : array) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        System.out.println("Even Numbers: ");
+        display(even);
+        System.out.println("Odd Numbers: ");
+        display(odd);
+    }
+
+    // Method to find two neighboring numbers with the smallest difference
+    public int findSmallestDifferenceIndex(int[] array) {
+        if (array.length < 2) {
+            System.out.println("Array must have at least two elements.");
+            return -1;
+        }
+
+        int minIndex = 0;
+        int minDiff = Math.abs(array[1] - array[0]);
+
+        for (int i = 1; i < array.length - 1; i++) {
+            int diff = Math.abs(array[i + 1] - array[i]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
 }
