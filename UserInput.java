79cd6c8e3d3e373b801f8d9952580faf5029907c
nidhi-builds <nidhipandit2907@gmail.com>
2025-02-03 22:36:02 +0530

    Add files via upload
    records the user input for the array

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+//UserInput.java
+
+import java.util.*;
+class UserInput{
+	int[] arrayInput(){
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[5];
+		System.out.println("Enter 5 numbers:");
+		
+		for (int i = 0; i< array.length; i++){
+			array[i] = scan.nextInt();
+                }
+		return array;
+	}
+}
