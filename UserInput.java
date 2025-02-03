dedef7822de45d0fc83b472c905e1cb7718b31d9
nidhi-builds <nidhipandit2907@gmail.com>
2025-02-03 23:12:30 +0530

    Add files via upload
    Refactored user input handling
    
    - Improved `arrayInput()` method to ensure clean user input handling.
    - Used Scanner to accept numbers and store them in an array.
    - Ensured consistency in input format for seamless integration with other functions.

--- UserInput.java
+++ UserInput.java
@@ -1,15 +1,18 @@
-//UserInput.java
+// UserInput.java
 
 import java.util.*;
-class UserInput{
-	int[] arrayInput(){
-		Scanner scan = new Scanner(System.in);
-		int[] array = new int[5];
-		System.out.println("Enter 5 numbers:");
-		
-		for (int i = 0; i< array.length; i++){
-			array[i] = scan.nextInt();
-                }
-		return array;
-	}
+
+class UserInput {
+    
+    // Method to take user input and store it in an array
+    int[] arrayInput() {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.println("Enter 5 numbers:");
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+        return array;
+    }
 }
