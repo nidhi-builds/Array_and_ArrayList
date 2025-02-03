d481c0ba07041dfd2cc72922126e0ef8d346fe1f
nidhi-builds <nidhipandit2907@gmail.com>
2025-02-03 23:13:14 +0530

    Add files via upload
    Updated main to include closest number search
    
    - Integrated `findSmallestDifferenceIndex()` to identify the index of 
      the first number in the closest pair.
    - Added a structured approach to process user input and call functions 
      for even/odd separation and closest pair search.
    - Included formatted output for clarity.

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
-//Main.java
+// Main.java
+// Name: Nidhi Pandit
+// PRN: 23070126081
+// Batch: B1
 
-class Main{
-	public static void main(String args[]){
-		UserInput in = new UserInput();
-		ArrayFunctions functions = new ArrayFunctions();
-		
-		functions.oddEven(in.arrayInput());
-
-     }
-
+class Main {
+    public static void main(String args[]) {
+        UserInput in = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
+        
+        int[] userArray = in.arrayInput();
+        
+        // Task 1: Separate even and odd numbers
+        functions.oddEven(userArray);
+        
+        // Task 2: Find the index of two neighboring numbers with the smallest difference
+        int minIndex = functions.findSmallestDifferenceIndex(userArray);
+        System.out.println("Index of the first number in the closest pair: " + minIndex);
+    }
 }
