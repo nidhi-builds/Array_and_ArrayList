ee1f850b331e54a5b49ff7a75e3b544344dbafbd
nidhi-builds <nidhipandit2907@gmail.com>
2025-02-03 22:37:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Main.java
+
+class Main{
+	public static void main(String args[]){
+		UserInput in = new UserInput();
+		ArrayFunctions functions = new ArrayFunctions();
+		
+		functions.oddEven(in.arrayInput());
+
+     }
+
+}
